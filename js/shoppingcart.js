a3d6f352dd2f76f55905537bce27e6596ca15700
eternal <490035790@qq.com>
2014-08-26 13:34:19 +0800

    使用模块定义
    使用模块定义

--- js/shoppingcart.js
+++ js/shoppingcart.js
@@ -1,23 +1,24 @@
-function cartControl($scope){
-	$scope.items = [ 
+var mycart = angular.module("mycart",[]);
+mycart.controller("cartControl",function($scope) {
+	var items=	[ 
 						{title:'paint pot1' , quantity:8, price:3.95},
 						{title:'paint pot2' , quantity:7, price:3.95},
 						{title:'paint pot3' , quantity:6, price:3.95}
-				   ];
-
+			   	];
+	$scope.items=items;
 	$scope.remove = function(index){
 		$scope.items.splice(index,1);
-	}
+	}	
+});
 
-	// easyDialog.open({
-	// 	container:{
-	// 		header : "提示",
-	// 		content : "是否要删除么？",
-	// 		yesFn : $scope.remove = function(index){
-	// 					$scope.items.splice(index,1);
-	// 				},
-	// 		noFn : true		
-	// 	}
-	// });
-		
-}
+// function cartControl($scope){
+// 	$scope.items = [ 
+// 						{title:'paint pot1' , quantity:8, price:3.95},
+// 						{title:'paint pot2' , quantity:7, price:3.95},
+// 						{title:'paint pot3' , quantity:6, price:3.95}
+// 				   ];
+
+// 	$scope.remove = function(index){
+// 		$scope.items.splice(index,1);
+// 	}	
+// }
