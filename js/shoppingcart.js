3088af4cce33c6c403e5013ddab038081020f2aa
eternal <490035790@qq.com>
2014-08-29 11:48:21 +0800

    格式化测试

--- js/shoppingcart.js
+++ js/shoppingcart.js
@@ -1,9 +1,9 @@
 var mycart = angular.module("mycart",[]);
 mycart.controller("cartControl",function($scope) {
 	var items=	[ 
-						{title:'paint pot1' , quantity:8, price:3.95},
-						{title:'paint pot2' , quantity:7, price:3.95},
-						{title:'paint pot3' , quantity:6, price:3.95}
+						{title:'paint pot1' , quantity:8, price:3.95 , day: new Date()},
+						{title:'paint pot2' , quantity:7, price:3.95 , day: new Date()},
+						{title:'paint pot3' , quantity:6, price:3.95 , day: new Date()}
 			   	];
 	$scope.items=items;
 	$scope.remove = function(index){
