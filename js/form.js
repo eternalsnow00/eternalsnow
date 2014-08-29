3357460a7bae213d793b36b77b14a7c48754f5be
eternal <490035790@qq.com>
2014-08-29 13:57:48 +0800

    form
    填入合法数字判断

--- js/form.js
+++ js/form.js
@@ -4,7 +4,20 @@ function formControl($scope){
 function stratControl($scope){
 	$scope.funding={ startingEstimate : 0 };
 	computeNeeded=function(){
-		$scope.funding.needed=$scope.funding.startingEstimate*10;
+		if(!/^(?!0[1-9])?\d+(\.\d{1,3})?$/.test($scope.funding.startingEstimate)){
+			easyDialog.open({
+			  	container : {
+			    	header : '提示',
+			    	content : '输入数字不合法',
+			   	 	noFn : true,
+			   	 	noText : "确认"
+			  	},
+			  	overlay : false
+			});
+			$scope.funding.startingEstimate=0;			
+		}else{
+			$scope.funding.needed=$scope.funding.startingEstimate*10;
+		}
 	};
 	$scope.$watch("funding.startingEstimate" , computeNeeded);
 }
